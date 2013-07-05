2ec0ffdc0d887d05266337cdc9462b0bae4352e8
Shzylo <shzylo66@gmail.com>
2013-07-05 13:22:12 -0500

    Create permissionExample.init.groovy

---
+++ examples/permissionExample.init.groovy
@@ -0,0 +1,21 @@
+// This is only for servers, really.
+on([playerJoined]) { //We will use playerJoined for this
+  type, event -> //initialize the event
+	
+	/**
+	 * playerData - gets the player's data info
+	 * 
+	 * get(String grant) - lets get the grant, you want to
+	 * put as your permission, 'grant.<permission' admin is
+	 * default for administrators
+	 * 
+	 * != null will check if the player does have the grant.
+	 */
+	if(playerData.get("grant.admin") != null) {
+		
+		//check out the addShellCommand Example for info on it.
+		addShellCommand(shell, "Hello", "Just a Test", null) {
+			console.echo("Hello World")
+		}
+	}
+}
