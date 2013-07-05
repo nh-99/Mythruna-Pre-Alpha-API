b674f2bb7344e8ea841c0dc78660a433e6d7b20b
Shzylo <shzylo66@gmail.com>
2013-07-04 21:48:08 -0500

    Fixed an error in the code and changed the imports.

--- examples/action.init.groovy
+++ examples/action.init.groovy
@@ -1,7 +1,7 @@
 // You NEED to import these to use the action.
 // Otherwise, it bugs out, and doesn't work.
-import mythruna.es.*
-import mythruna.script.*
+import mythruna.script.ActionType
+import mythruna.script.ToolActions
 
 /**
  * - group:(String groupName) - makes a new category for the tool, specify the name with
@@ -17,18 +17,18 @@ action(group:"Information Grabber", name:"Block Information", type:ActionType.Bl
 	console.echo("Block Type: " + type + ", Location: " + loc)	
 }
 
-on([playerJoin]) { //this will add the action command to the player when they join.
+on([playerJoined]) { //this will add the action command to the player when they join.
 	type, event -> // lets register this event..
-	
+
 	def refs = [] //an arraylist to store all actions.
-	
+
 	//Just in case, the player already has other tools,
 	//lets take the current tools they have, so we don't lose them.
 	ToolActions existing = player[ToolActions.class]
-	
+
 	//adds our action to the arraylist 'refs', with the category name, and the tool name.
 	refs += actions.getRef("Information Grabber", "Block Information")
-	
+
 	//Now we give it to the player!
 	player << new ToolActions(refs, existing)	
 }
